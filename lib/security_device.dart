1061d0cb4a293ca90ea8deb66b6b8e5c0a4e7e09
skhsiao <steven-KP.hsiao@nanshan.com.tw>
2023-05-26 12:15:59 +0800

    add comments.

--- lib/security_device.dart
+++ lib/security_device.dart
@@ -3,18 +3,22 @@ import 'package:flutter/services.dart';
 
 class SecurityDevice {
 
+  /// this package's channel
   static const MethodChannel _channel = MethodChannel('security_device');
 
+  /// Quickly confirm that the device is security, just use isSecurityDevice.
   static Future<bool> get isSecurityDevice async {
     bool? isSecurityDevice = await _channel.invokeMethod<bool>('isSecurityDevice');
     return isSecurityDevice ?? true;
   }
 
+  /// check Android is rooted, check iOS is jailbroken(JB)
   static Future<bool> get isJailbreakOrRoot async {
     bool? jailbroken = await _channel.invokeMethod<bool>('isJailbreakOrRoot');
     return jailbroken ?? true;
   }
 
+  /// Android: ADB over USB is enabled, iOS: is Run In Emulator.
   static Future<bool> get isDeveloperMode async {
     bool? developerMode = await _channel.invokeMethod<bool>('isDeveloperMode');
     return developerMode ?? true;
